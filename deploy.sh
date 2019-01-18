3915705a6b51197bb2a20fc6df90baa216a74727
Craig McNamara <craig@caring.com>
2019-01-17 22:31:14 -0800

    move to docs

--- deploy.sh
+++ deploy.sh
@@ -58,7 +58,7 @@ parse_args() {
   # vars should be declared here, with sane defaults if applicable.
 
   # Source directory & target branch.
-  deploy_directory=build
+  deploy_directory=docs
   deploy_branch=gh-pages
 
   #if no user identity is already set in the current git environment, use this:
